85dc378d6893ffbca21deab1150ac68e797f8c1b
Flak15 <flak160@gmail.com>
2020-11-18 17:47:52 +0300

    settings reload

--- .build/dist/components/Main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Main = () => {
-    return (React.createElement("div", { className: "container" },
-        React.createElement("div", { className: "row justify-content-md-center" },
-            React.createElement("div", { className: "col-6" },
-                React.createElement("h1", null, "Main")))));
-};
-export default Main;
--- .build/dist/config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-    baseURL: process.env.NODE_ENV === 'production' ? 'http://invest.terminal.mobi:4000' : 'http://localhost:4000'
-};
-export default config;
